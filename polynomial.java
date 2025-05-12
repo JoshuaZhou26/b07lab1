9b640adafe67295a7e538377493097fb9c4ead19
JoshuaZhou25 <joshua.zhoumz@gmail.com>
2025-05-12 14:22:58 -0400

    test

---
+++ polynomial.java
@@ -0,0 +1,15 @@
+package b07lab1
+
+public class Polynomial {
+	double[] coeff;
+	
+	public Polynomial() {
+		this.coeff = {0};
+	}
+	
+	public Polynomial(double[] coeff) {
+		this.coeff = coeff;
+	}
+	
+	public add()
+}
