49102796900488d1bab71761dc3d17c26eee8aa0
JoshuaZhou26 <joshua.zhoumz@icloud.com>
2025-05-12 21:38:54 -0400

    added Polynomial.java and Driver.java

--- polynomial.java
+++ polynomial.java
@@ -1,15 +1,44 @@
-package b07lab1
-
 public class Polynomial {
 	double[] coeff;
 	
 	public Polynomial() {
-		this.coeff = {0};
+		this.coeff = new double[]{0};
 	}
 	
 	public Polynomial(double[] coeff) {
 		this.coeff = coeff;
 	}
 	
-	public add()
+	public Polynomial add(Polynomial a) {
+		int xlen = this.coeff.length;
+		int ylen = a.coeff.length;
+		int len = Math.max(xlen, ylen);
+		double[] sum = new double[len];
+
+		for (int i = 0; i < len; i++) {
+			double x,y;
+			if (i >= xlen) x = 0;
+			else x = this.coeff[i];
+			if (i >= ylen) y = 0;
+			else y = a.coeff[i];
+			
+			sum[i] = x + y;
+		}
+		
+		return new Polynomial(sum);
+	}
+	
+	public double evaluate(double x) {
+		int power = 0;
+		double result = 0;
+		for (double i : this.coeff) {
+			result += i * Math.pow(x, power);
+			power++;
+		}
+		return result;
+	}
+	
+	public boolean hasRoot(double root) {
+		return this.evaluate(root) != 0;
+	}
 }
